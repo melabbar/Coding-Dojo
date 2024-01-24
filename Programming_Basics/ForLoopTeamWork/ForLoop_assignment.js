016ca6d62a07310a464d718be8ed805b0b5f0aa4
melabbar <melabbar.17@gmail.com>
2024-01-24 12:26:12 +0100

    assignment

---
+++ Programming_Basics/ForLoopTeamWork/ForLoop_assignment.js
@@ -0,0 +1,7 @@
+var year = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+for (var i = 0; i < year.length; i++) {
+    for (var j = 0; i < year.length; j + 10) {
+        console.log("Happy Decade for JS")
+    }
+    console.log(year[i] + " Js is Awesome!")
+}
---
+++ Programming_Basics/code.js
@@ -0,0 +1,7 @@
+// This variable is called LikeCount and starts ar zero
+var likeCount = 0;
+// the function below is caled increaseLikes()
+function increaseLikes() {
+    // Inside the function has an operation that adds the variable by one 
+    likeCount = likeCount + 1;
+}
