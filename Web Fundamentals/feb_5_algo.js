e290d5fefd929690cf7fc1724f67cb4e3f66c7fd
melabbar <melabbar.17@gmail.com>
2024-02-07 19:03:25 +0100

    assignment

---
+++ Web Fundamentals/feb_5_algo.js
@@ -0,0 +1,9 @@
+function slice(start, end) {
+    var arr = [1, 2, 3, 4, 5]
+    for (var i = start; i < end - 1; i++) {
+        if (start = end) {
+            console.log(arr[start - end])
+        }
+    }
+}
+console.log(slice(1, 4))
