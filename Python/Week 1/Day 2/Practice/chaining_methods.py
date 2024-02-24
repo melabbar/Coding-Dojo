253def4a31e125a8f7237ca0171e1ca001fb3acd
melabbar <melabbar.17@gmail.com>
2024-02-24 18:30:00 +0100

    practice

---
+++ Python/Week 1/Day 2/Practice/chaining_methods.py
@@ -0,0 +1,54 @@
+class User():
+
+    def __init__(self, first_name,last_name, email, age, is_rewards_member=False, gold_card_points=0):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
+        self.age=age
+        self.is_rewards_member=is_rewards_member
+        self.gold_card_points=gold_card_points
+
+
+    def display_info(self):
+        print(f"My name is {self.first_name} {self.last_name}")
+        print(f"I am {self.age} years old")
+        print(f"My email is: {self.email} ")
+        print(f"Rewards member: {self.is_rewards_member}")
+        print(f"Gold Card points: {self.gold_card_points}")
+        return self
+    
+    def enroll(self):
+        if self.is_rewards_member:
+            print("Already a user.")
+            return False
+        else:
+            self.is_rewards_member=True
+        self.is_rewards_member=True
+        self.gold_card_points=200
+        return self
+    
+    def spend_points(self, amount):
+        self.gold_card_points -= amount
+        if self.gold_card_points < amount:
+            print(f"Whoa {self.first_name}!! Stop it your spending too much!")
+        return self
+
+        
+
+
+info= User("Muftah", "Elabbar", "a@a.com", 33, False, 0)
+info1 = User("Yassin", "Khalifa","y@y.com", 25, False, 0)
+info2 = User("Maria", "Cain","c@c.com", 22, False, 0)
+
+
+print("----------------------------")
+info.display_info().enroll().spend_points(50).display_info()
+
+
+print("----------------------------")
+info1.display_info().enroll().spend_points(80).display_info()
+
+print("----------------------------")
+info2.display_info().enroll().spend_points(201).display_info()
+
+
