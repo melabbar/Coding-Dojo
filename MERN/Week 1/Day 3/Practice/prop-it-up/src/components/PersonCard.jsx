3731cc311fd9c8fa4aee4118de54b90b800ed4f1
Muftah Elabbar <108698280+melabbar@users.noreply.github.com>
2024-04-13 04:28:16 +0100

    practice

---
+++ MERN/Week 1/Day 3/Practice/prop-it-up/src/components/PersonCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PersonCard = (props) => {
+    return (
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <h3>Age: {props.age}</h3>
+            <h3>Hair Color: {props.hairColor}</h3>
+
+        </div>
+    )
+}
+export default PersonCard;
