9a2404a5cd435cce9c1df64e678a8b15125c437e
Muftah Elabbar <108698280+melabbar@users.noreply.github.com>
2024-05-13 14:24:48 +0100

    java01

---
+++ JAVA/Week 01/Day 01/Practice/FirstJava/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Muftah Elabbar!");
+        System.out.println("I am 33 years old!");
+        System.out.println("My hometown is Blabla...");
+    }
+}
