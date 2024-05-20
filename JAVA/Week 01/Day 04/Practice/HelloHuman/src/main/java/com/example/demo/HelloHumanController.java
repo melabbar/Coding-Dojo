ac05c10f8f55929664a1eb3330f389ed1651ee97
Muftah Elabbar <108698280+melabbar@users.noreply.github.com>
2024-05-20 17:08:59 +0100

    PracW01D04

---
+++ JAVA/Week 01/Day 04/Practice/HelloHuman/src/main/java/com/example/demo/HelloHumanController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloHumanController {
+	@RequestMapping("/")
+	public String index() {
+		return "Hello Human";
+	}
+	
+	@RequestMapping("/name")
+	public String name(@RequestParam(value="q",required = false) String searchQuery) {
+		return "Hello " + searchQuery;
+	}
+}
